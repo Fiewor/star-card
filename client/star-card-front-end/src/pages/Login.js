68d5d64c9762275830e7935b630da2e05213508b
IsraelChidera <israelchidera54@gmail.com>
2021-11-10 18:06:38 +0100

    Login/Signup feature added

---
+++ client/star-card-front-end/src/pages/Login.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import '../components/Login.css';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <>
+            <section className="login">
+                <div className="login-container">
+                    <div className="login-form">
+                        <div className="login-form-div">
+                            <h1>Welcome Back</h1>
+                            <form 
+                                action=""
+                                className=""
+                            >
+                                <div>
+                                    <input 
+                                        type="email"
+                                        placeholder="Email"
+                                        required
+                                    />
+                                </div>
+
+                                <div>
+                                    <input 
+                                        type="password"
+                                        placeholder="Password"
+                                        required
+                                    />
+                                    <p className="forgot-pass">
+                                        <a href="#">
+                                            Forgot password ?
+                                        </a>
+                                    </p>
+                                </div>
+
+                                <div>
+                                    <button className="login-btn">
+                                        Sign In
+                                    </button>
+                                </div>
+
+                                <p className="sign-up-link">
+                                    You don't have an account? <Link to="/signup">
+                                        Sign up here . . .
+                                    </Link>  
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div className="login-bg">
+                        <div>
+                            <h3>
+                                Star Card
+                            </h3>
+                            <p>
+                                secure your 100% risk assesment data entry
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Login
---
+++ client/star-card-front-end/src/pages/Signup.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import '../components/Signup.css';
+import { Link } from 'react-router-dom';
+
+const Signup = () => {
+    return (
+        <>
+            <section className="signup">
+                <div className="signup-container">
+                    <div className="signup-form">
+                        <div className="signup-form-div">
+                            <h1>Sign Up</h1>
+                            <form 
+                                action=""
+                                className=""
+                            >
+                                <div>
+                                    <input 
+                                        type="text"
+                                        placeholder="Name"
+                                        required
+                                    />
+                                </div>
+
+                                <div>
+                                    <input 
+                                        type="text"
+                                        placeholder="Organization"
+                                        required
+                                    />
+                                </div>
+
+                                <div>
+                                    <input 
+                                        type="email"
+                                        placeholder="Email"
+                                        required
+                                    />
+                                </div>
+
+                                <div>
+                                    <input 
+                                        type="password"
+                                        placeholder="Password"
+                                        required
+                                    />
+                                    <p className="forgot-pass">
+                                        <a href="#">
+                                            Forgot password ?
+                                        </a>
+                                    </p>
+                                </div>
+
+                                <div>
+                                    <button className="signup-btn">
+                                        Sign up
+                                    </button>
+                                </div>
+
+                                <p className="sign-up-link">
+                                    Already have an account? <Link to="/">
+                                        Login . . .
+                                    </Link>  
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div className="signup-bg">
+                        <div>
+                            <h3>
+                                Star Card
+                            </h3>
+                            <p>
+                                secure your 100% risk assesment data entry
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Signup
